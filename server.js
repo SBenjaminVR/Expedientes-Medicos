eed6bb6d5fc018aa8613a69b7e1200b0e7ab130b
Benjamín Valdez <benjamin.vr.1998@gmail.com>
2020-06-02 00:47:35 -0500

    AAAAAAHHH

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-const mongoose = require('mongoose')
-const dotenv = require('dotenv');
-dotenv.config({path: './config.env'});
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
 
-const app = require('./app');
+const app = express()
 
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
-mongoose.connect(DB, {useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false}).then(() => {
-    console.log("DB Connection successful")
+//here we are configuring dist to serve app files
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, function (req, res) {
+	res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`App running on port ${port}...`)
-})
-
+const port = process.env.PORT || 8080
+app.listen(port)
+console.log(`app is listening on port: ${port}`)
